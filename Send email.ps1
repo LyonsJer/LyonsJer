8c35d7b7962459e322a6a5625bf96348245e5292
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-05-17 14:11:40 +0300

    Update Send email.ps1

--- Send email.ps1
+++ Send email.ps1
@@ -4,18 +4,18 @@
 #צירך לבדוק שהמיילים תקינים - המייל הראשון מבוסס על ההנחה שהמייל הוא השם משתמש בלי קידומת דהיינו השתי אותיות הראשונות
 # $MAIL.TO ולהזין ידנית המייל המבוקש במשתנה $EMAIL אם לא, אפשר להוריד את המשתנה 
 $user = $env:Username 
-
-$email = $user.Substring(2) + "@moin.gov.il"
-
+#deletes first 2 letters from Username and adds domain to it
+$email = $user.Substring(2) + "@domain.co.il"
+#Creates New email
 $Mail = $Outlook.CreateItem(0)
-
+#sends mail to email address form $email variable
 $Mail.To = $email
 
 #כאן תרשום מייל חיצוני
-$Mail.CC = "jl8439246@gmail.com"
-
+$Mail.CC = "<make sure email is in quotations>"
+#sets subject of email
 $Mail.Subject = "subject"
-
+#sets body of email
 $Mail.Body = "testing"
-
+#sends email
 $Mail.Send()
