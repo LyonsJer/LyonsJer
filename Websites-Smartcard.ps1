097ccefc2042a8a709efe061723aa337225e0729
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-05-17 14:44:03 +0300

    Update Websites-Smartcard.ps1

--- Websites-Smartcard.ps1
+++ Websites-Smartcard.ps1
@@ -1,27 +1,25 @@
-﻿$obj = New-Object -ComObject Wscript.Shell
+﻿#Imports object that allows for keystrokes in powershell
+ # complete list of options at this website http://msdn.microsoft.com/en-us/library/office/aa202943%28v=office.10%29.aspx
+ #This allows to enter preset pin code automatically
+ #Keystrokes would have to be changed for each instance
+ 
+ $obj = New-Object -ComObject Wscript.Shell
 
-Read-Host -Prompt "To Check Saar File Sharing and Mercava Press Enter, Check that Smart card is inserted and PIN is set to 1212"
+Read-Host -Prompt "To Check Websites Press Enter, Check that Smart card is inserted and PIN is set to ***"
 
-#בודק מערכת סער באקספלורר ומרכבה נוכחות באדג 
-$ie_procinfo = Start-Process iexplore -ArgumentList 'https://moin.saar.gov.il/saar2500' -PassThru
+ 
+$ie_procinfo = Start-Process iexplore -ArgumentList 'https://website.com' -PassThru
 $ie_procid = $ie_procinfo.Id
 echo $ie_procid
+#sleep command is important to allow website to load before proceeding to next keystroke
 Sleep 15
 
+#Keystroke to navigate to pin code entry
 $obj.SendKeys("{TAB}{ENTER}")
 Sleep 2
-$obj.SendKeys("1212{ENTER}")
+#pincode is entered
+$obj.SendKeys("****{ENTER}")
 
-Sleep 5
-
-$edge_procinfo = Start-Process msedge -ArgumentList "https://ofan.merkava.gov.il/Merkava" -PassThru
-$edge_procid = $edge_procinfo.Id
-echo $edge_procid
-
-Sleep 15
-$obj.SendKeys("{TAB}{TAB}{TAB}{ENTER}")
-Sleep 2
-$obj.SendKeys("1212{ENTER}")
 
 
 
