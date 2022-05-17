d4301910a12b6066a08a8dafe7f457d3fff5f057
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-05-17 14:07:25 +0300

    Add files via upload
    Automate sending email to internal and external address

---
+++ Send email.ps1
@@ -0,0 +1,21 @@
+﻿$Outlook = New-Object -ComObject Outlook.Application
+
+
+#צירך לבדוק שהמיילים תקינים - המייל הראשון מבוסס על ההנחה שהמייל הוא השם משתמש בלי קידומת דהיינו השתי אותיות הראשונות
+# $MAIL.TO ולהזין ידנית המייל המבוקש במשתנה $EMAIL אם לא, אפשר להוריד את המשתנה 
+$user = $env:Username 
+
+$email = $user.Substring(2) + "@moin.gov.il"
+
+$Mail = $Outlook.CreateItem(0)
+
+$Mail.To = $email
+
+#כאן תרשום מייל חיצוני
+$Mail.CC = "jl8439246@gmail.com"
+
+$Mail.Subject = "subject"
+
+$Mail.Body = "testing"
+
+$Mail.Send()
