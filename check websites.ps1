7beadf00c00cd9f304d7a4d4fa2a386ffba79b22
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-05-17 14:13:18 +0300

    Add files via upload
    Checks website listed in file

---
+++ check websites.ps1
@@ -0,0 +1,5 @@
+﻿Import-Csv '\\pnimsrv3\jmjeremy\Websites to Check.csv' | ForEach-Object {
+    $request = Invoke-WebRequest -URi $($_.URL) -UseDefaultCredentials | Select-Object -ExpandProperty StatusDescription
+Write-Host $($_.Website) $request
+}
+
