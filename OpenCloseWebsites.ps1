6be1bcad7b712835145ea8c091c8cf430b510b94
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-05-17 14:17:23 +0300

    Add files via upload
    Opens Websites from file in Edge and prompts user to close

---
+++ OpenCloseWebsites.ps1
@@ -0,0 +1,16 @@
+﻿# WEBSITES.TXT בודק אתרים שונים לפי שנקבע בקובץ
+
+$Websites = Get-Content '\\pnimsrv3\jmjeremy\Websites.txt' 
+
+ForEach($Website in $Websites){
+   
+$edge_procinfo = Start-Process msedge -ArgumentList "$Website" -PassThru
+$edge_procid = $edge_procinfo.Id
+
+
+}
+
+Read-Host -Prompt "Press Enter to close Microsoft Edge"
+
+TaskKill /IM msedge.exe /F
+
