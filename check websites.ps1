fdc93d74affd171676fb01c7d50e3638ddef7028
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-05-17 14:16:16 +0300

    Update check websites.ps1

--- check websites.ps1
+++ check websites.ps1
@@ -1,4 +1,8 @@
-﻿Import-Csv '\\pnimsrv3\jmjeremy\Websites to Check.csv' | ForEach-Object {
+#Checks website and reports if 200 status code was recieved
+
+#CSV file has 2 columns one with website name -column called website and URL -column called URL
+
+Import-Csv '\\pnimsrv3\jmjeremy\Websites to Check.csv' | ForEach-Object {
     $request = Invoke-WebRequest -URi $($_.URL) -UseDefaultCredentials | Select-Object -ExpandProperty StatusDescription
 Write-Host $($_.Website) $request
 }
