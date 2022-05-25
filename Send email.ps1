f383fd700866acee0b72a5e47dcdf9ae6dfcb431
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-05-25 16:37:43 +0300

    Update Send email.ps1

--- Send email.ps1
+++ Send email.ps1
@@ -1,21 +1,51 @@
-﻿$Outlook = New-Object -ComObject Outlook.Application
-
-
-#צירך לבדוק שהמיילים תקינים - המייל הראשון מבוסס על ההנחה שהמייל הוא השם משתמש בלי קידומת דהיינו השתי אותיות הראשונות
-# $MAIL.TO ולהזין ידנית המייל המבוקש במשתנה $EMAIL אם לא, אפשר להוריד את המשתנה 
+﻿#צירך לבדוק שהמיילים תקינים - המייל הראשון מבוסס על ההנחה שהמייל הוא השם משתמש בלי קידומת דהיינו השתי אותיות הראשונות
+# $MAIL.TO ולהזין ידנית המייל המבוקש במשתנה $EMAIL אם לא, אפשר להוריד את המשתנה
+ 
 $user = $env:Username 
-#deletes first 2 letters from Username and adds domain to it
-$email = $user.Substring(2) + "@domain.co.il"
-#Creates New email
+
+$email = $user.Substring(2) + "@moin.gov.il"
+
+$outlook = New-Object -ComObject Outlook.Application
+
 $Mail = $Outlook.CreateItem(0)
-#sends mail to email address form $email variable
+
 $Mail.To = $email
 
-#כאן תרשום מייל חיצוני
-$Mail.CC = "<make sure email is in quotations>"
-#sets subject of email
-$Mail.Subject = "subject"
-#sets body of email
-$Mail.Body = "testing"
-#sends email
+# כאן תרשום מייל חיצוני חשוב להגדיר את תיבת המייל לשלוח מייל אוטומטי בחזרה
+$Mail.CC = "jl8439246@gmail.com"
+
+#כאן מגדירים את הנושא
+ 
+$Mail.Subject = "Test Mail"
+
 $Mail.Send()
+
+ # לפי עומס ברשת אולי צריך להגדיל את הזמן 
+#  שמחכה עד שזה סורק את אאוטלוק למיילים שנשלחו
+
+Sleep 60
+
+$outlook = New-Object -ComObject Outlook.Application
+
+$olFolders = “Microsoft.Office.Interop.Outlook.olDefaultFolders” -as [type]
+
+$namespace = $outlook.GetNameSpace(“MAPI”)
+
+$folder = $namespace.getDefaultFolder($olFolders::olFolderInBox)
+
+#הנושאים של המיילים שהמערכת סורק צריך להיות זהים עם הנושא שנקבע למעלה
+#קודם זה מחפש את המייל שנשלח למייל פנימי ואז המייל שנשלח אוטומטי מהמייל החיצוני
+$emailsArrived = $folder.items | Where-Object {($_.Subject -eq "Test mail") -or ($_.Subject -eq 'Re: Test Mail');}
+
+$EmailsToDelete = $emailsArrived.Count
+
+#בודק כאן עם יש רק שתי מיילים שהגיע ונמצא בתוך המערכת
+#echo $EmailsToDelete
+
+If($EmailsToDelete -eq 2){
+Write-Host "Mail Sent Successfully" -ForegroundColor Green;
+$emailsArrived.Delete()
+}
+else{
+    Write-Host "Mail Test Failed" -ForegroundColor Red
+}
