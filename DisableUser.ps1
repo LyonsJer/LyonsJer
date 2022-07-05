1d13d1fdda125c6340bd143b416dbde62897a156
LyonsJer <105717558+LyonsJer@users.noreply.github.com>
2022-07-05 11:43:32 +0300

    Update DisableUser.ps1

--- DisableUser.ps1
+++ DisableUser.ps1
@@ -7,23 +7,40 @@ $nonAlphaChars = 5
 $password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
 $secPw = ConvertTo-SecureString -String $password -AsPlainText -Force
 
-#Prompts Username to enter Username to Disable
-$UsertoDisable = Read-Host "Please Enter Username to Disable"
+Import-Csv C:\UsersToDisable.csv | ForEach-Object{
+
+#Creates Variable of User and Groups to be deleted
+$User = Get-ADUser -Identity $_.UserstoDisable 
+
+$Groups = Get-ADPrincipalGroupMembership -Identity $_.UserstoDisable `
+    | Where-Object {$_.distinguishedName -CLike "*OU=Outlook-Contacts,OU=Jeremy,DC=Lyons,DC=Server"} `
+    
+#Combines Variables to one object for export
+$list = Foreach ($Group in $Groups){
+New-Object PSobject -Property @{
+ User = $User
+ Groups = $Group
+}}
+ 
+#Exports to CSV with Users name
+$UsersName = Get-ADUser -Identity $_.UserstoDisable | Select-Object -ExpandProperty Name
+
+$List | Export-Csv -NoTypeInformation C:\$UsersName.csv
 
 #Disables User
-Disable-ADAccount -Identity $UsertoDisable
+Disable-ADAccount -Identity $_.UserstoDisable
 #Resets Password
-Set-ADAccountPassword -Identity $UsertoDisable -NewPassword $secPw -Reset
+Set-ADAccountPassword -Identity $_.UserstoDisable -NewPassword $secPw -Reset
 
 #Searches for all Outlook-Contact groups that User is a member of 
-$OutlookGroups = Get-ADPrincipalGroupMembership -Identity $UsertoDisable `
-    | Where-Object {$_.distinguishedName -CLike "*OU=Outlook-Contacts,OU=Jeremy,DC=Lyons,DC=Server"} `
-    | Select-Object -ExpandProperty distinguishedName
-
+$OutlookGroups = $Groups | Select-Object -ExpandProperty distinguishedName 
+    
 #Remove User from each Outlook Contact Group
 ForEach ($OutlookGroup in $OutlookGroups){
-    Remove-ADPrincipalGroupMembership -Identity $UsertoDisable -MemberOf $OutlookGroup
+    Remove-ADPrincipalGroupMembership -Identity $_.UserstoDisable -MemberOf $OutlookGroup -Confirm:$false
 }
 
 #Moves User to Disabled
-Get-ADUSer -Identity $UsertoDisable | Move-ADObject -TargetPath "OU=Disabled Users,OU=Jeremy,DC=Lyons,DC=Server"
+Get-ADUSer -Identity $_.UserstoDisable | Move-ADObject -TargetPath "OU=Disabled Users,OU=Jeremy,DC=Lyons,DC=Server"
+
+}
